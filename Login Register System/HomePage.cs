01deb78391b0f85f4b233028705e5ef9a2ac53ef
M. Novaldy Pratama H <novaldypratama@mail.ugm.ac.id>
2022-09-19 18:56:06 +0700

    Login Register Layout
    test

---
+++ Login Register System/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Register_System
+{
+    public partial class HomePage : Form
+    {
+        private string username;
+        public HomePage(string username)
+        {
+            this.username = username;
+            InitializeComponent();
+        }
+
+        private void HomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
