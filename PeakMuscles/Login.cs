c57e6c677bc39f106a57ef2de79c10707b802cbd
ChristinaAngraeniP <panellah.a11christina@gmail.com>
2022-09-19 20:25:02 +0700

    Penambahan beberapa class(1)

---
+++ PeakMuscles/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakMuscles
+{
+    class Login
+    {
+        private string username;
+        private string password;
+        
+        public string Username { get => username; }
+        public string Password { get => password; }
+
+        public Login()
+        {
+
+        }
+
+        public Login(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
---
+++ PeakMuscles/Register.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakMuscles
+{
+    class Register
+    {
+        private string _fullName;
+        private int _ageID;
+        private string _username;
+        private string _password;
+        private string _confirmpassword;
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set { _fullName = value; }
+        }
+        public int AgeID
+        {
+            get { return _ageID; }
+        }
+        public string UserName
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public string ConfirmPassword
+        {
+            get { return _confirmpassword; }
+            set { _confirmpassword = value; }
+        }
+
+        public Register(string UserName, string Password)
+        {
+            _username = UserName;
+            _password = Password;
+        }
+    }
+}
